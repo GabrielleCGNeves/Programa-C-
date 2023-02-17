516ab02c84dac05d685718487ad119c7b6668db9
GabrielleCGNeves <gabriellecarvalhoneves@gmail.com>
2023-02-17 19:50:00 -0300

    add imag

--- ProjetoAula02/Form1.Designer.cs
+++ ProjetoAula02/Form1.Designer.cs
@@ -33,13 +33,15 @@ namespace ProjetoAula02
             this.rbtBulbassauro = new System.Windows.Forms.RadioButton();
             this.rbtSquirtle = new System.Windows.Forms.RadioButton();
             this.rbtCharmander = new System.Windows.Forms.RadioButton();
+            this.pictureBoxPokemon = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPokemon)).BeginInit();
             this.SuspendLayout();
             // 
             // lblDescricao
             // 
             this.lblDescricao.AutoSize = true;
             this.lblDescricao.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDescricao.Location = new System.Drawing.Point(29, 26);
+            this.lblDescricao.Location = new System.Drawing.Point(29, 9);
             this.lblDescricao.Name = "lblDescricao";
             this.lblDescricao.Size = new System.Drawing.Size(250, 24);
             this.lblDescricao.TabIndex = 0;
@@ -49,11 +51,10 @@ namespace ProjetoAula02
             // 
             this.rbtBulbassauro.AutoSize = true;
             this.rbtBulbassauro.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rbtBulbassauro.Location = new System.Drawing.Point(33, 121);
+            this.rbtBulbassauro.Location = new System.Drawing.Point(33, 132);
             this.rbtBulbassauro.Name = "rbtBulbassauro";
             this.rbtBulbassauro.Size = new System.Drawing.Size(114, 21);
             this.rbtBulbassauro.TabIndex = 1;
-            this.rbtBulbassauro.TabStop = true;
             this.rbtBulbassauro.Text = "Bulbassauro";
             this.rbtBulbassauro.UseVisualStyleBackColor = true;
             // 
@@ -61,19 +62,19 @@ namespace ProjetoAula02
             // 
             this.rbtSquirtle.AutoSize = true;
             this.rbtSquirtle.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rbtSquirtle.Location = new System.Drawing.Point(33, 94);
+            this.rbtSquirtle.Location = new System.Drawing.Point(33, 93);
             this.rbtSquirtle.Name = "rbtSquirtle";
             this.rbtSquirtle.Size = new System.Drawing.Size(82, 21);
             this.rbtSquirtle.TabIndex = 2;
-            this.rbtSquirtle.TabStop = true;
             this.rbtSquirtle.Text = "Squitle";
             this.rbtSquirtle.UseVisualStyleBackColor = true;
             // 
             // rbtCharmander
             // 
             this.rbtCharmander.AutoSize = true;
+            this.rbtCharmander.Checked = true;
             this.rbtCharmander.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rbtCharmander.Location = new System.Drawing.Point(33, 67);
+            this.rbtCharmander.Location = new System.Drawing.Point(33, 54);
             this.rbtCharmander.Name = "rbtCharmander";
             this.rbtCharmander.Size = new System.Drawing.Size(106, 21);
             this.rbtCharmander.TabIndex = 3;
@@ -81,11 +82,21 @@ namespace ProjetoAula02
             this.rbtCharmander.Text = "Charmander";
             this.rbtCharmander.UseVisualStyleBackColor = true;
             // 
+            // pictureBoxPokemon
+            // 
+            this.pictureBoxPokemon.Image = global::ProjetoAula02.Properties.Resources.charmander;
+            this.pictureBoxPokemon.Location = new System.Drawing.Point(217, 54);
+            this.pictureBoxPokemon.Name = "pictureBoxPokemon";
+            this.pictureBoxPokemon.Size = new System.Drawing.Size(475, 475);
+            this.pictureBoxPokemon.TabIndex = 4;
+            this.pictureBoxPokemon.TabStop = false;
+            // 
             // FormPokemon
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 451);
+            this.ClientSize = new System.Drawing.Size(704, 541);
+            this.Controls.Add(this.pictureBoxPokemon);
             this.Controls.Add(this.rbtCharmander);
             this.Controls.Add(this.rbtSquirtle);
             this.Controls.Add(this.rbtBulbassauro);
@@ -93,6 +104,7 @@ namespace ProjetoAula02
             this.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "FormPokemon";
             this.Text = "O Programa Pokemon";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPokemon)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -104,6 +116,7 @@ namespace ProjetoAula02
         private System.Windows.Forms.RadioButton rbtBulbassauro;
         private System.Windows.Forms.RadioButton rbtSquirtle;
         private System.Windows.Forms.RadioButton rbtCharmander;
+        private System.Windows.Forms.PictureBox pictureBoxPokemon;
     }
 }
 
