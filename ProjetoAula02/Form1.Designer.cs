93ba0a3dc9732aec30d55278c21da04df4c737de
GabrielleCGNeves <gabriellecarvalhoneves@gmail.com>
2023-02-20 14:43:32 -0300

    new design

--- ProjetoAula02/Form1.Designer.cs
+++ ProjetoAula02/Form1.Designer.cs
@@ -33,81 +33,220 @@ namespace ProjetoAula02
             this.rbtBulbassauro = new System.Windows.Forms.RadioButton();
             this.rbtSquirtle = new System.Windows.Forms.RadioButton();
             this.rbtCharmander = new System.Windows.Forms.RadioButton();
+            this.rbtMagnemite = new System.Windows.Forms.RadioButton();
+            this.rbtAbra = new System.Windows.Forms.RadioButton();
+            this.rbtDiglett = new System.Windows.Forms.RadioButton();
+            this.rbtGastly = new System.Windows.Forms.RadioButton();
+            this.rbtGrowlithe = new System.Windows.Forms.RadioButton();
+            this.rbtPonyta = new System.Windows.Forms.RadioButton();
+            this.rbtCaterpie = new System.Windows.Forms.RadioButton();
+            this.lblNome = new System.Windows.Forms.Label();
             this.pictureBoxPokemon = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPokemon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblDescricao
             // 
             this.lblDescricao.AutoSize = true;
+            this.lblDescricao.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(0)))), ((int)(((byte)(22)))));
             this.lblDescricao.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDescricao.Location = new System.Drawing.Point(29, 9);
+            this.lblDescricao.Location = new System.Drawing.Point(34, 30);
             this.lblDescricao.Name = "lblDescricao";
-            this.lblDescricao.Size = new System.Drawing.Size(250, 24);
+            this.lblDescricao.Size = new System.Drawing.Size(314, 32);
             this.lblDescricao.TabIndex = 0;
             this.lblDescricao.Text = "Escolha seu Pokemon:";
+            this.lblDescricao.Click += new System.EventHandler(this.lblDescricao_Click);
             // 
             // rbtBulbassauro
             // 
             this.rbtBulbassauro.AutoSize = true;
-            this.rbtBulbassauro.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rbtBulbassauro.Location = new System.Drawing.Point(33, 132);
+            this.rbtBulbassauro.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(0)))), ((int)(((byte)(22)))));
+            this.rbtBulbassauro.Font = new System.Drawing.Font("Consolas", 11F, System.Drawing.FontStyle.Bold);
+            this.rbtBulbassauro.Location = new System.Drawing.Point(62, 138);
             this.rbtBulbassauro.Name = "rbtBulbassauro";
-            this.rbtBulbassauro.Size = new System.Drawing.Size(114, 21);
+            this.rbtBulbassauro.Size = new System.Drawing.Size(141, 26);
             this.rbtBulbassauro.TabIndex = 1;
             this.rbtBulbassauro.Text = "Bulbassauro";
-            this.rbtBulbassauro.UseVisualStyleBackColor = true;
+            this.rbtBulbassauro.UseVisualStyleBackColor = false;
             this.rbtBulbassauro.CheckedChanged += new System.EventHandler(this.rbtBulbassauro_CheckedChanged);
             // 
             // rbtSquirtle
             // 
             this.rbtSquirtle.AutoSize = true;
-            this.rbtSquirtle.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rbtSquirtle.Location = new System.Drawing.Point(33, 93);
+            this.rbtSquirtle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(0)))), ((int)(((byte)(22)))));
+            this.rbtSquirtle.Font = new System.Drawing.Font("Consolas", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtSquirtle.Location = new System.Drawing.Point(62, 106);
             this.rbtSquirtle.Name = "rbtSquirtle";
-            this.rbtSquirtle.Size = new System.Drawing.Size(82, 21);
+            this.rbtSquirtle.Size = new System.Drawing.Size(101, 26);
             this.rbtSquirtle.TabIndex = 2;
             this.rbtSquirtle.Text = "Squitle";
-            this.rbtSquirtle.UseVisualStyleBackColor = true;
+            this.rbtSquirtle.UseVisualStyleBackColor = false;
             this.rbtSquirtle.CheckedChanged += new System.EventHandler(this.rbtSquirtle_CheckedChanged);
             // 
             // rbtCharmander
             // 
             this.rbtCharmander.AutoSize = true;
+            this.rbtCharmander.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(0)))), ((int)(((byte)(22)))));
+            this.rbtCharmander.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.rbtCharmander.Checked = true;
-            this.rbtCharmander.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rbtCharmander.Location = new System.Drawing.Point(33, 54);
+            this.rbtCharmander.Font = new System.Drawing.Font("Consolas", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtCharmander.Location = new System.Drawing.Point(62, 74);
             this.rbtCharmander.Name = "rbtCharmander";
-            this.rbtCharmander.Size = new System.Drawing.Size(106, 21);
+            this.rbtCharmander.Size = new System.Drawing.Size(131, 26);
             this.rbtCharmander.TabIndex = 3;
             this.rbtCharmander.TabStop = true;
             this.rbtCharmander.Text = "Charmander";
-            this.rbtCharmander.UseVisualStyleBackColor = true;
+            this.rbtCharmander.UseVisualStyleBackColor = false;
             this.rbtCharmander.CheckedChanged += new System.EventHandler(this.rbtCharmander_CheckedChanged);
+            // 
+            // rbtMagnemite
+            // 
+            this.rbtMagnemite.AutoSize = true;
+            this.rbtMagnemite.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.rbtMagnemite.Font = new System.Drawing.Font("Consolas", 11F, System.Drawing.FontStyle.Bold);
+            this.rbtMagnemite.Location = new System.Drawing.Point(62, 330);
+            this.rbtMagnemite.Name = "rbtMagnemite";
+            this.rbtMagnemite.Size = new System.Drawing.Size(121, 26);
+            this.rbtMagnemite.TabIndex = 5;
+            this.rbtMagnemite.Text = "Magnemite";
+            this.rbtMagnemite.UseVisualStyleBackColor = false;
+            this.rbtMagnemite.CheckedChanged += new System.EventHandler(this.rbtJigglypuff_CheckedChanged);
+            // 
+            // rbtAbra
+            // 
+            this.rbtAbra.AutoSize = true;
+            this.rbtAbra.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.rbtAbra.Font = new System.Drawing.Font("Consolas", 11F, System.Drawing.FontStyle.Bold);
+            this.rbtAbra.Location = new System.Drawing.Point(62, 298);
+            this.rbtAbra.Name = "rbtAbra";
+            this.rbtAbra.Size = new System.Drawing.Size(71, 26);
+            this.rbtAbra.TabIndex = 6;
+            this.rbtAbra.Text = "Abra";
+            this.rbtAbra.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.rbtAbra.UseVisualStyleBackColor = false;
+            this.rbtAbra.CheckedChanged += new System.EventHandler(this.rbtSandshrew_CheckedChanged);
+            // 
+            // rbtDiglett
+            // 
+            this.rbtDiglett.AutoSize = true;
+            this.rbtDiglett.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.rbtDiglett.Font = new System.Drawing.Font("Consolas", 11F, System.Drawing.FontStyle.Bold);
+            this.rbtDiglett.Location = new System.Drawing.Point(62, 266);
+            this.rbtDiglett.Name = "rbtDiglett";
+            this.rbtDiglett.Size = new System.Drawing.Size(101, 26);
+            this.rbtDiglett.TabIndex = 7;
+            this.rbtDiglett.Text = "Diglett";
+            this.rbtDiglett.UseVisualStyleBackColor = false;
+            this.rbtDiglett.CheckedChanged += new System.EventHandler(this.rbtEkans_CheckedChanged);
+            // 
+            // rbtGastly
+            // 
+            this.rbtGastly.AutoSize = true;
+            this.rbtGastly.BackColor = System.Drawing.Color.Black;
+            this.rbtGastly.Font = new System.Drawing.Font("Consolas", 11F, System.Drawing.FontStyle.Bold);
+            this.rbtGastly.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.rbtGastly.Location = new System.Drawing.Point(62, 362);
+            this.rbtGastly.Name = "rbtGastly";
+            this.rbtGastly.Size = new System.Drawing.Size(91, 26);
+            this.rbtGastly.TabIndex = 8;
+            this.rbtGastly.Text = "Gastly";
+            this.rbtGastly.UseVisualStyleBackColor = false;
+            this.rbtGastly.CheckedChanged += new System.EventHandler(this.rbtRattata_CheckedChanged);
+            // 
+            // rbtGrowlithe
+            // 
+            this.rbtGrowlithe.AutoSize = true;
+            this.rbtGrowlithe.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.rbtGrowlithe.Font = new System.Drawing.Font("Consolas", 11F, System.Drawing.FontStyle.Bold);
+            this.rbtGrowlithe.Location = new System.Drawing.Point(62, 234);
+            this.rbtGrowlithe.Name = "rbtGrowlithe";
+            this.rbtGrowlithe.Size = new System.Drawing.Size(121, 26);
+            this.rbtGrowlithe.TabIndex = 9;
+            this.rbtGrowlithe.Text = "Growlithe";
+            this.rbtGrowlithe.UseVisualStyleBackColor = false;
+            this.rbtGrowlithe.CheckedChanged += new System.EventHandler(this.rbtPidgey_CheckedChanged);
+            // 
+            // rbtPonyta
+            // 
+            this.rbtPonyta.AutoSize = true;
+            this.rbtPonyta.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.rbtPonyta.Font = new System.Drawing.Font("Consolas", 11F, System.Drawing.FontStyle.Bold);
+            this.rbtPonyta.Location = new System.Drawing.Point(62, 202);
+            this.rbtPonyta.Name = "rbtPonyta";
+            this.rbtPonyta.Size = new System.Drawing.Size(91, 26);
+            this.rbtPonyta.TabIndex = 10;
+            this.rbtPonyta.Text = "Ponyta";
+            this.rbtPonyta.UseVisualStyleBackColor = false;
+            this.rbtPonyta.CheckedChanged += new System.EventHandler(this.rbtWeedle_CheckedChanged);
+            // 
+            // rbtCaterpie
+            // 
+            this.rbtCaterpie.AutoSize = true;
+            this.rbtCaterpie.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.rbtCaterpie.Font = new System.Drawing.Font("Consolas", 11F, System.Drawing.FontStyle.Bold);
+            this.rbtCaterpie.Location = new System.Drawing.Point(62, 170);
+            this.rbtCaterpie.Name = "rbtCaterpie";
+            this.rbtCaterpie.Size = new System.Drawing.Size(121, 26);
+            this.rbtCaterpie.TabIndex = 11;
+            this.rbtCaterpie.Text = "Caterpie ";
+            this.rbtCaterpie.UseVisualStyleBackColor = false;
+            this.rbtCaterpie.CheckedChanged += new System.EventHandler(this.rbtCaterpie_CheckedChanged);
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.BackColor = System.Drawing.Color.Transparent;
+            this.lblNome.Location = new System.Drawing.Point(188, 428);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(160, 17);
+            this.lblNome.TabIndex = 12;
+            this.lblNome.Text = "Gabrielle C G Neves";
             // 
             // pictureBoxPokemon
             // 
-            this.pictureBoxPokemon.Image = global::ProjetoAula02.Properties.Resources.charmander;
-            this.pictureBoxPokemon.Location = new System.Drawing.Point(217, 54);
+            this.pictureBoxPokemon.Image = global::ProjetoAula02.Properties.Resources.card_charmander;
+            this.pictureBoxPokemon.Location = new System.Drawing.Point(370, 43);
             this.pictureBoxPokemon.Name = "pictureBoxPokemon";
-            this.pictureBoxPokemon.Size = new System.Drawing.Size(475, 475);
+            this.pictureBoxPokemon.Size = new System.Drawing.Size(270, 371);
             this.pictureBoxPokemon.TabIndex = 4;
             this.pictureBoxPokemon.TabStop = false;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ProjetoAula02.Properties.Resources.sim;
+            this.pictureBox1.Location = new System.Drawing.Point(-7, -69);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(789, 601);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
             // FormPokemon
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(704, 541);
+            this.ClientSize = new System.Drawing.Size(652, 445);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.rbtCaterpie);
+            this.Controls.Add(this.rbtPonyta);
+            this.Controls.Add(this.rbtGrowlithe);
+            this.Controls.Add(this.rbtGastly);
+            this.Controls.Add(this.rbtDiglett);
+            this.Controls.Add(this.rbtAbra);
+            this.Controls.Add(this.rbtMagnemite);
             this.Controls.Add(this.pictureBoxPokemon);
             this.Controls.Add(this.rbtCharmander);
             this.Controls.Add(this.rbtSquirtle);
             this.Controls.Add(this.rbtBulbassauro);
             this.Controls.Add(this.lblDescricao);
+            this.Controls.Add(this.pictureBox1);
             this.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "FormPokemon";
             this.Text = "O Programa Pokemon";
+            this.Load += new System.EventHandler(this.FormPokemon_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPokemon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -120,6 +259,15 @@ namespace ProjetoAula02
         private System.Windows.Forms.RadioButton rbtSquirtle;
         private System.Windows.Forms.RadioButton rbtCharmander;
         private System.Windows.Forms.PictureBox pictureBoxPokemon;
+        private System.Windows.Forms.RadioButton rbtMagnemite;
+        private System.Windows.Forms.RadioButton rbtAbra;
+        private System.Windows.Forms.RadioButton rbtDiglett;
+        private System.Windows.Forms.RadioButton rbtGastly;
+        private System.Windows.Forms.RadioButton rbtGrowlithe;
+        private System.Windows.Forms.RadioButton rbtPonyta;
+        private System.Windows.Forms.RadioButton rbtCaterpie;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- ProjetoAula02/Form1.cs
+++ ProjetoAula02/Form1.cs
@@ -19,17 +19,62 @@ namespace ProjetoAula02
 
         private void rbtCharmander_CheckedChanged(object sender, EventArgs e)
         {
-            pictureBoxPokemon.Image = Properties.Resources.charmander;
+            pictureBoxPokemon.Image = Properties.Resources.card_charmander;
         }
 
         private void rbtSquirtle_CheckedChanged(object sender, EventArgs e)
         {
-            pictureBoxPokemon.Image = Properties.Resources.squirtle;
+            pictureBoxPokemon.Image = Properties.Resources.card_squirtle;
         }
 
         private void rbtBulbassauro_CheckedChanged(object sender, EventArgs e)
         {
-            pictureBoxPokemon.Image = Properties.Resources.bulbasaur;
+            pictureBoxPokemon.Image = Properties.Resources.card_bulbasaur;
+        }
+
+        private void rbtCaterpie_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBoxPokemon.Image = Properties.Resources.card_caterpie;
+        }
+
+        private void rbtWeedle_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBoxPokemon.Image = Properties.Resources.card_ponyta;
+        }
+
+        private void rbtPidgey_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBoxPokemon.Image = Properties.Resources.card_growlithe;
+        }
+
+        private void rbtRattata_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBoxPokemon.Image = Properties.Resources.card_gastly;
+        }
+
+        private void rbtEkans_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBoxPokemon.Image = Properties.Resources.card_diglett;
+        }
+
+        private void rbtSandshrew_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBoxPokemon.Image = Properties.Resources.card_abra;
+        }
+
+        private void rbtJigglypuff_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBoxPokemon.Image = Properties.Resources.card_magnemite;
+        }
+
+        private void FormPokemon_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblDescricao_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
